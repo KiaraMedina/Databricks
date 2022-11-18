096043a832bc03cffe007a3fca16338dada840f6
KiaraMedina <KiaraMedina>
2022-11-18 01:23:42 +0000

    commend transformations

--- ingesta/0.Indest_all_files.py
+++ ingesta/0.Indest_all_files.py
@@ -1,5 +1,15 @@
 # Databricks notebook source
-v_result = dbutils.notebook.run("1.Ingest circuits.csv file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+dbutils.widgets.text("p_data_source","")
+v_data_soruce = dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date","2021-03-21")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("1.Ingest circuits.csv file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -7,7 +17,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("2.Ingest races.csv file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+v_result = dbutils.notebook.run("2.Ingest races.csv file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -15,7 +25,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("3.Ingest constructor.json file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+v_result = dbutils.notebook.run("3.Ingest constructor.json file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -23,7 +33,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("4.ingest_drivers_file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+v_result = dbutils.notebook.run("4.ingest_drivers_file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -31,7 +41,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("5.ingest_result.csv file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+# v_result = dbutils.notebook.run("5.ingest_result.csv file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -39,7 +49,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("6.Ingest pit_stops.json file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+# v_result = dbutils.notebook.run("6.Ingest pit_stops.json file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -47,7 +57,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("7.Ingest_lap_times_file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+# v_result = dbutils.notebook.run("7.Ingest_lap_times_file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
@@ -55,7 +65,7 @@ v_result
 
 # COMMAND ----------
 
-v_result = dbutils.notebook.run("8.Ingest_qualifying_file",0,{"p_data_source":"Ergast API", "p_file_date":"2021-03-21"})
+# v_result = dbutils.notebook.run("8.Ingest_qualifying_file",0,{"p_data_source": v_data_soruce, "p_file_date":v_file_date})
 
 # COMMAND ----------
 
